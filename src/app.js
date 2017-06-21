2880686dc29a6dfeaa5839fc7e3f4bd08698495e
Jules Bonnard <julesbonnard@gmail.com>
2017-06-21 19:14:04 +0200

    iptc-parser works

--- src/app.js
+++ src/app.js
@@ -1,21 +1,9 @@
 const express = require("express");
 const app = express();
 
-const multer = require("multer");
-const storage = multer.memoryStorage();
-const upload = multer({ storage, limits: { fileSize: "15mb" } });
+const iptcParser = require("./iptc-parser");
 
-const iptc = require("node-iptc");
-
-app.get("/", function(req, res) {
-  res.send("Hello World!");
-});
-
-app.post("/", upload.single("image"), (req, res, next) => {
-  const iptcData = iptc(req.file.buffer);
-  if (iptcData) return res.json(iptcData);
-  next("Error while parsing IPTC");
-});
+app.use("/", iptcParser);
 
 app.use(express.static("public"));
 
