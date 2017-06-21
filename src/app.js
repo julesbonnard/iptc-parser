8823d96f596f2012912804df44ce1423f3ef7e7d
Jules Bonnard <julesbonnard@gmail.com>
2017-06-21 17:51:55 +0200

    add multer and node-iptc

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const app = express();
 
+const multer = require("multer");
+const storage = multer.memoryStorage();
+const upload = multer({ storage, limits: { fileSize: "15mb" } });
+
+const iptc = require("node-iptc");
+
 app.get("/", function(req, res) {
   res.send("Hello World!");
+});
+
+app.post("/", upload.single("image"), (req, res, next) => {
+  const iptcData = iptc(req.file.buffer);
+  if (iptcData) return res.json(iptcData);
+  next("Error while parsing IPTC");
 });
 
 app.use(express.static("public"));
