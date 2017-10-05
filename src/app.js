c401425ef44a4709c1b87d4a500b9b01620f55eb
Jules Bonnard <julesbonnard@gmail.com>
2017-10-05 18:07:56 +0200

    add Host env

--- src/app.js
+++ src/app.js
@@ -6,9 +6,11 @@ const iptcParser = require("./iptc-parser");
 
 app.use(cors());
 
-app.use("/", iptcParser);
+const HOST = process.env.HOST || "/";
 
-app.use(express.static("public"));
+app.use(HOST, iptcParser);
+
+app.use(HOST, express.static("public"));
 
 app.listen(3000, function() {
   console.log("IPTC-parser listening on port 3000!");
