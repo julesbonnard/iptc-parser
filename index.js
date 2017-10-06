05a000f6661c81585624ad79424175d773bd2e88
Jules Bonnard <julesbonnard@gmail.com>
2017-10-06 12:25:52 +0200

    prettier + gitignore

--- index.js
+++ index.js
@@ -1,50 +1,70 @@
-const busboy = require('busboy')
-const iptcParser = require('node-iptc')
+const busboy = require("busboy");
+const iptcParser = require("node-iptc");
 
 const headers = {
-  'Content-Type': 'application/json',
-  'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Methods': 'OPTIONS, POST',
-  'Access-Control-Allow-Headers': 'Content-Type'
-}
+  "Content-Type": "application/json",
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "OPTIONS, POST",
+  "Access-Control-Allow-Headers": "Content-Type"
+};
 
 function handler(event, context) {
-  const contentType = event.headers['Content-Type'] || event.headers['content-type']
-  const bb = new busboy({ headers: { 'content-type': contentType }})
+  const contentType =
+    event.headers["Content-Type"] || event.headers["content-type"];
+  const bb = new busboy({ headers: { "content-type": contentType } });
 
   const results = [];
-  bb.on('file', function (fieldname, file, filename, encoding, mimetype) {
-    console.log('File [%s]: filename=%j; encoding=%j; mimetype=%j', fieldname, filename, encoding, mimetype)
-    
-    file.buff = []
+  bb
+    .on("file", function(fieldname, file, filename, encoding, mimetype) {
+      console.log(
+        "File [%s]: filename=%j; encoding=%j; mimetype=%j",
+        fieldname,
+        filename,
+        encoding,
+        mimetype
+      );
 
-    file
-    .on('data', function(data) {
-        console.log('File [%s] got %d bytes', fieldname, data.length)
-        this.buff[this.buff.length] = data
-    })
-    .on('end', function() {
-        console.log('File [%s] Finished', fieldname)
-        const buffer = Buffer.concat(this.buff)
-        const fileParsed = {
-          name: filename,
-          mimetype: fileData.mimetype,
-          iptc: iptcParser(buffer)
-        }
-        results.push(iptc)
-    })
-  })
-  .on('field', (fieldname, val) =>console.log('Field [%s]: value: %j', fieldname, val))
-  .on('finish', () => {
-    console.log('Done parsing form!')
-    context.succeed({ statusCode: 200, body: JSON.stringify(results), headers, "isBase64Encoded": false })
-  })
-  .on('error', err => {
-    console.log('failed', err)
-    context.fail({ statusCode: 500, body: err, headers, "isBase64Encoded": false })
-  })
+      file.buff = [];
 
-  bb.end(event.body, event.isBase64Encoded ? 'base64' : 'binary')
+      file
+        .on("data", function(data) {
+          console.log("File [%s] got %d bytes", fieldname, data.length);
+          this.buff[this.buff.length] = data;
+        })
+        .on("end", function() {
+          console.log("File [%s] Finished", fieldname);
+          const buffer = Buffer.concat(this.buff);
+          const fileParsed = {
+            name: filename,
+            mimetype: fileData.mimetype,
+            iptc: iptcParser(buffer)
+          };
+          results.push(iptc);
+        });
+    })
+    .on("field", (fieldname, val) =>
+      console.log("Field [%s]: value: %j", fieldname, val)
+    )
+    .on("finish", () => {
+      console.log("Done parsing form!");
+      context.succeed({
+        statusCode: 200,
+        body: JSON.stringify(results),
+        headers,
+        isBase64Encoded: false
+      });
+    })
+    .on("error", err => {
+      console.log("failed", err);
+      context.fail({
+        statusCode: 500,
+        body: err,
+        headers,
+        isBase64Encoded: false
+      });
+    });
+
+  bb.end(event.body, event.isBase64Encoded ? "base64" : "binary");
 }
 
-module.exports = { handler }
+module.exports = { handler };
